8172c01975612ea1ae8b353df663aeac91b9920b
akaforey <akafore1@binghamton.edu>
2022-02-06 11:45:56 -0500

    flask and html input working well
    image is now broken

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask
 import pickle
+import pandas as pd
 # Use pickle to load in the pre-trained model.
 # with open(f'model/bike_model_xgboost.pkl', 'rb') as f:
 #     model = pickle.load(f)
@@ -11,21 +12,29 @@ app = flask.Flask(__name__, template_folder='templates')
 def main():
     if flask.request.method == 'GET':
         return(flask.render_template('main.html'))
+
     if flask.request.method == 'POST':
+        print(flask.request.form)
         salary = flask.request.form['salary']
         gender = flask.request.form['gender']
         age = flask.request.form['age']
-        input_variables = pd.DataFrame([[salary,gender,age]],
-                                       columns=['salary', 'gender', 'age'],
-                                       # dtype=float
-                                       )
+        role = flask.request.form['role']
+        military = flask.request.form['military']
+        state = flask.request.form['state']
+
+        comment = flask.request.form.get('comment')
+
+        # input_variables = pd.DataFrame([[salary,gender,age]],
+        #                                columns=['salary', 'gender', 'age'],
+        #                                # dtype=float
+        #                                )
         # prediction = model.predict(input_variables)[0]
-        return flask.render_template('main.html',
-                                     original_input={'salary':salary,
-                                                     'gender':gender,
-                                                     'age':age},
+        return flask.render_template('main.html')
+                                     # original_input={'salary':salary,
+                                     #                 'gender':gender,
+                                     #                 'age':age},
                                      # result=prediction,
-                                     )
+                                     # )
 
 
 if __name__ == '__main__':
